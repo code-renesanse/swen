83ce90ef25370876e59543418a4d9a0b9bd0d01d
TilenOkretic <tilen.okretic@gmail.com>
2022-12-24 03:02:40 +0100

    :sparkles: linting fix

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: [
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['import', '@typescript-eslint'],
+  rules: {},
+};
