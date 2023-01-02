10b6e84aef4c6267b8f2fcd40f3008081abebdd9
TilenOkretic <tilen.okretic@gmail.com>
2023-01-03 00:37:41 +0100

    :tada: dock-wrapper from div to ul

--- src/dom/wrapper.ts
+++ src/dom/wrapper.ts
@@ -1,14 +1,14 @@
 import { createElement } from './create';
 
 export const createEmptyWrapper = (): HTMLDivElement => {
-  const _wrapper = createElement('div', 'wrapper');
-  const _wrapperContainer = createElement('div', 'wrapper-container');
-  const _dockWrapper = createElement('div', 'dock-wrapper');
-  const _langButtonHolder = createElement('div', 'language-button-holder');
+  const wrapper = createElement('div', 'wrapper');
+  const wrapperContainer = createElement('div', 'wrapper-container');
+  const dockWrapper = createElement('ul', 'dock-wrapper');
+  const langButtonHolder = createElement('div', 'language-button-holder');
 
-  _wrapperContainer.appendChild(_dockWrapper);
-  _wrapperContainer.appendChild(_langButtonHolder);
+  wrapperContainer.appendChild(dockWrapper);
+  wrapperContainer.appendChild(langButtonHolder);
 
-  _wrapper.appendChild(_wrapperContainer);
-  return _wrapper;
+  wrapper.appendChild(wrapperContainer);
+  return wrapper;
 };
